04cbcefdec4457c7f0a478aa6083f70e957496f6
Andrea D'Amico <andrea.damico@gmail.com>
2012-11-30 14:50:02 +0100

    website attribute fix in profile

--- app/helpers/refinery/memberships/members_helper.rb
+++ app/helpers/refinery/memberships/members_helper.rb
@@ -6,6 +6,7 @@ module Refinery
           rejected_keys = %w(id username encrypted_password current_sign_in_at last_sign_in_at current_sign_in_ip last_sign_in_ip sign_in_count remember_created_at reset_password_token reset_password_sent_at created_at updated_at enabled seen rejected confirmation_token confirmed_at confirmation_sent_at)
           v = v.blank? ? "<em>not specified</em>".html_safe : v
           v = l(v) if k == "member_until"
+          v = link_to(v, v, target: "_blank") if k == "website"
           yield(k, v) unless rejected_keys.include? k
         end
       end
